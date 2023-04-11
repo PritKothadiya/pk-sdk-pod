0ca7bed8d3c3ed5f653f537973809d6ff0499d18
Prit Kothadiya <prithkothadiya@Aurionpros-MacBook-Pro.local>
2023-04-12 00:22:08 +0530

    First commit

---
+++ PKSDK/PKDemoSDK.swift
@@ -0,0 +1,17 @@
+//
+//  PKDemoSDK.swift
+//  PKSDK
+//
+//  Created by Prit Kothadiya on 11/04/23.
+//
+
+import Foundation
+
+
+public struct PKDemoSDK{
+    
+    public static func testFunc(){
+        print("Hello There!")
+    }
+    
+}
---
+++ build/PKSDK.xcframework/ios-arm64/PKSDK.framework/Headers/PKSDK.h
@@ -0,0 +1,18 @@
+//
+//  PKSDK.h
+//  PKSDK
+//
+//  Created by Prit Kothadiya on 11/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PKSDK.
+FOUNDATION_EXPORT double PKSDKVersionNumber;
+
+//! Project version string for PKSDK.
+FOUNDATION_EXPORT const unsigned char PKSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PKSDK/PublicHeader.h>
+
+
